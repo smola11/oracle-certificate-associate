7954544933ab9748ae6ffc4ac08037bcc64479c3
Smolarz <mjsz@gft.com>
2018-07-23 14:41:37 +0200

    public class inside another public class does not compile - requires separate file

---
+++ src/com/company/javaBuildingBlocks1/Animal.java
@@ -0,0 +1,14 @@
+package com.company.javaBuildingBlocks1;
+
+public class Animal {
+}
+
+class Animal2 {
+
+}
+
+// NOT COMPILING - public class should be declared in separate file "Animal3.java"
+
+//public class Animal3 {
+//
+//}
