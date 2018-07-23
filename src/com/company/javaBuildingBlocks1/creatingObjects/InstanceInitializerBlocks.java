43787e352676a6ce7018440ffba333e4cb99bbed
Smolarz <mjsz@gft.com>
2018-07-23 15:51:34 +0200

    Example of instance initializer

---
+++ src/com/company/javaBuildingBlocks1/creatingObjects/InstanceInitializerBlocks.java
@@ -0,0 +1,16 @@
+package com.company.javaBuildingBlocks1.creatingObjects;
+
+public class InstanceInitializerBlocks {
+
+    // Below we have 3 code blocks (and only 1 instance initializer)
+    public static void main(String[] args) {
+        {
+            System.out.println("Feathers");
+        }
+    }
+
+    // instance initializer (code block outside the method)
+    {
+        System.out.println("Snowy");
+    }
+}
